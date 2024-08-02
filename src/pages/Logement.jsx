f515d34f3c0a08758ed012a2c01e8a4ce3213dd7
RemKiovo <epaulais.remi@gmail.com>
2024-08-02 11:27:33 +0200

    Added all the necessary info for logement. TODO: Styling

--- src/pages/Logement.jsx
+++ src/pages/Logement.jsx
@@ -26,6 +26,28 @@ function Logement() {
       <div id="slideshow-container">
         <Slideshow pictures={logement.pictures} />
       </div>
+      <div>
+        <h1>{logement.title}</h1>
+        <p>{logement.description}</p>
+        <h2>{logement.host.name}</h2>
+        <img src={logement.host.picture} alt={logement.host.name} />
+        <p>{logement.rating}</p>
+        <h3>{logement.location}</h3>
+        <ul>
+          {logement.equipments.map((equipment) => (
+            <li key={equipment}>{equipment}</li>
+          ))}
+        </ul>
+        <ul
+          style={{
+            paddingTop: '10px',
+          }}
+        >
+          {logement.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   ) : null
 }
